9815c9536f2176d0e3a0c537b2c96e6ea0208a2c
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2020-09-19 15:57:35 +0300

    Модель получения списка заявок

--- upload/admin/model/extension/module/ocn_callback.php
+++ upload/admin/model/extension/module/ocn_callback.php
@@ -1,2 +1,11 @@
 <?php
-class ModelExtensionModuleOcnCallBack extends Model {}
+class ModelExtensionModuleOcnCallBack extends Model {
+	public function list() {
+		$query = '';
+		$query .= "SELECT cb.*, cbs.name as status_name";
+		$query .= " FROM " . DB_PREFIX . "ocn_callback cb";
+		$query .= " LEFT JOIN " . DB_PREFIX . "ocn_callback_status cbs ON (cb.callback_status_id = cbs.callback_status_id  AND cbs.language_id = '" . (int)$this->config->get('config_language_id') . "')";
+		
+		return $this->db->query($query)->rows;
+	}
+}
