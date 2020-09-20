3436a3302142c0ae43fbc51e4cf78e538da56840
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2020-09-20 21:44:16 +0300

    Добавил в модель метод рассчета кол-ва заявок

--- upload/admin/model/extension/module/ocn_callback.php
+++ upload/admin/model/extension/module/ocn_callback.php
@@ -1,13 +1,20 @@
 <?php
 class ModelExtensionModuleOcnCallBack extends Model {
-	public function list() {
+	public function list($data = []) {
 		$query = "";
 		$query .= " SELECT cb.*, cbs.name as status_name";
 		$query .= " FROM " . DB_PREFIX . "ocn_callback cb";
 		$query .= " LEFT JOIN " . DB_PREFIX . "ocn_callback_status cbs ON (cb.callback_status_id = cbs.callback_status_id  AND cbs.language_id = '" . (int)$this->config->get('config_language_id') . "')";
 		$query .= " ORDER BY cb.callback_id DESC";
+		$query .= " LIMIT " . (int)$data['start'] . "," . (int)$data['limit'];
 		
 		return $this->db->query($query)->rows;
+	}
+	
+	public function total() {
+		$query = "SELECT COUNT(*) AS total FROM " . DB_PREFIX . "ocn_callback";
+		
+		return $this->db->query($query)->row['total'];
 	}
 	
 	public function update($data) {
